14571058efe1e174b6aeb9e45d00d41f69591766
Kevin Jensen <jensenk28@yahoo.com>
2020-01-29 11:49:41 -0800

    starting over, deleted components, going to make it more organized

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,36 +1,19 @@
 import React, {useState} from 'react';
 import './App.css';
 
-import Form from './components/Form';
-import Users from './components/Users';
+
 
 
 function App() {
 
-const [info, setInfo]=useState([
-  {
-    id: 1,
-    username: 'Kevin Jensen',
-    email: 'kevinjensen1133@yahoo.com',
-    role: 'Full Stack Web Developer'
-  }
-]);
-const addNewInfo = user => {
-      const newUser = {
-        id: Date.now(),
-        username: user.username,
-        email: user.email,
-        role: user.role
-      };
-      setInfo([...info, newUser]);
 
   return (
     <div className="App">
        
-       
+       <h1>hello</h1>
       
     </div>
   );
 }
-}
+
 export default App;
